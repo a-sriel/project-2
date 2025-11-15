c17dd53abf9b097a88898b2ac47b6fb02c72c210
a-sriel <rilescentauri@gmail.com>
2025-11-14 20:32:05 -0600

    Implemented outline for teller and customer functions

--- devlog.md
+++ devlog.md
@@ -9,3 +9,5 @@ I will probably start off by including most of the logic in the provided thread_
 2025-11-14 20:16
 
 This session, I'm going to implement some barebones logic for the customer and teller threads inside the thread_code program. I'm not planning to do much testing just yet; I just want to get a skeleton down so that I don't get lost in the weeds.
+
+I added some teller and customer functions. Within the functions, I simply modified the provided threadCode function that uses semaphores. I also added some global variables for things like, the total amount of money in the safe, etc. I'm not sure if this idea is going to work yet, but at least I have something down.
--- thread_code.cpp
+++ thread_code.cpp
@@ -5,6 +5,12 @@
 int gCount = 0;
 Semaphore gLock(1);
 
+int tCount = 0;
+Semaphore tLock(3);
+
+int cCount = 0;
+Semaphore custLock(50);
+
 void threadCode(int i) {
     gLock.wait();
     std::cout << "Thread " << i << " has count " << gCount << std::endl;
@@ -12,8 +18,67 @@ void threadCode(int i) {
     gLock.signal();
 }
 
-int main() {
+void teller(int i) {
+    int randomInt;
+    bool ready;
+    tLock.wait();
+    std::cout << "Thread " << i << " has count " << tCount << std::endl;
+    tCount++;
+    tLock.signal();
 
+    // ready
+    // wait for customer
+    // receive customer signal -> ask for transaction
+    // go to safe
+    // talk to customer
+    // wait for customer to leave
+}
+
+void customer(int i) {
+    custLock.wait();
+    std::cout << "Thread " << i << " has count " << cCount << std::endl;
+    cCount++;
+    custLock.signal();
+
+    // generates random behavior; 0 or 1
+    // where 0 = withdraw, 1 = deposit
+    bool action = rand() % 2;
+    // generate wait from 0-100
+    int wait = rand() % 101;
+    if (doorOccupancy > 2) {
+        // enter
+    } else {
+        // wait
+    }
+
+    // join line
+    // give id
+    // wait for teller
+    // give transaction
+    // wait for teller to finish
+    // leave
+}
+
+int doorOccupancy = 0; // max 2
+
+int customerBalance = 0;
+int safeBalance = 999;
+
+bool withdraw(int amount) {
+    if (amount > safeBalance) {
+        return false;
+    }
+
+    safeBalance - amount;
+    return true;
+}
+
+void deposit(int amount) {
+    customerBalance += amount;
+}
+
+int main() {
+/*
     std::thread threads[5];
 
     for(int i=0; i<5;i++) {
@@ -23,6 +88,32 @@ int main() {
     for(int i=0; i<5; i++) {
         threads[i].join();
     }
+
+    return 0;
+*/
+
+     // teller
+    std::thread tellers[3];
+
+    for (int i = 0; i < 3; i++) {
+        tellers[i] = std::thread(teller, i);
+    }
+
+    for (int i = 0; i < 3; i++) {
+        tellers[i].join();
+    }
+
+    // customer
+    std::thread customers[50];
+
+    for (int i = 0; i < 50; i++) {
+        customers[i] = std::thread(customer, i);
+    }
+
+    for (int i = 0; i < 50; i++) {
+        customers[i].join();
+    }
+
 
     return 0;
 }
